ed1e39ca4ac23e15eab8185ba24823308f163ef0
James Viviers <jprogdev14@gmail.com>
2018-08-28 22:20:38 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h2>Hi, my name is James Viviers</h2>
+    <h2>Looking really forward attending the online hackathon and meeting new people :)</h2>
+  </div>
+)
+
+export default IndexPage
